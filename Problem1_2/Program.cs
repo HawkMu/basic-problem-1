b8bd345356bfbfd275d08ec1831b9bee7e52a2fb
タカムラ カズキ 18TE402 <18te402@ueno.daiichi-koudai.ac.jp>
2022-08-04 19:56:10 +0900

    プロジェクト ファイルを追加します。

---
+++ Problem1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*変更前
+             * Console.Write("123");
+             *Console.Write("456");
+             *Console.WriteLine("789");
+             */
+            Console.WriteLine("123456789");
+        }
+    }
+}
---
+++ Problem1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*修正前
+             * Console.WritLine("こんにちは");
+             */
+            Console.WriteLine("こんにちは");
+        }
+    }
+}
